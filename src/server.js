39570f0ff9477744ad4f2cb3a90de07fbafcafbd
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-04-12 23:15:59 -0300

    Estrutura back-end

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log(ok);
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect(
+    "mongodb+srv://omnistack:omnistack@cluster0-ep1za.mongodb.net/test?retryWrites=true",
+    {
+        useNewUrlParser: true
+    }
+)
+
+app.use((req, res) => {
+    req.io = io;
+
+    return next();
+})
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(3000);
